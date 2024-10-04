d6fc2035c5d0f330e6cf67194e179e0abeecfd3e
jessica joseph <jessicajoseph2019@yahoo.com>
2024-10-04 03:22:22 +0100

    create elements, append, remove,  nextsibling and previous sibling dom methods exercises

--- dom.js
+++ dom.js
@@ -42,8 +42,62 @@ Your task is to select all spans, iterate over them,
 and assign them each one of the colors from the colors array. 
 The first span should be red, the second should be orange, etc.*/
 
-const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']; 
-const spans = document.querySelectorAll('h1 span');
-for(let i =  0; i <spans.length; i++){
-    spans[i].style.color =  colors[i]
+const colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
+const spans = document.querySelectorAll("h1 span");
+for (let i = 0; i < spans.length; i++) {
+  spans[i].style.color = colors[i];
+}
+
+/*Write a script that adds the class "active" to a button element.*/
+
+const button = document.querySelector("button").classList.add("active");
+
+/*Manually remove the class "highlight" from all <p> elements when the script runs.*/
+const allParagraphElements = document.querySelectorAll("p");
+for (let paragraph of allParagraphElements) {
+  paragraph.classList.remove("highlight");
+}
+
+/*Check if a <div> element with the ID "myDiv" has the class "hidden" 
+and log a message to the console depending on whether the class exists.*/
+
+const div = document.querySelector("#myDiv").classList.contains("hidden");
+if (!div) {
+  console.log("No hidden class exists");
+} else {
+  console.log("There is a class called hidden");
+}
+
+/*Toggle the class "dark-mode" on the <body> tag when the script runs.*/
+
+const body = document.querySelector("body").classList.toggle("dark-mode");
+
+/*Create exactly 100 new button elements
+Each button must have some text inside of it (it doesn't matter what)
+Each button must be appended inside the container div. */
+
+const container = document.querySelector("#container");
+for (let i = 1; i <= 100; i++) {
+  const button = document.createElement("button");
+  button.innerText = "Loop";
+  container.append(button);
+}
+
+/*Write a script that creates a new <p> element with the text "Hello, World!", 
+then appends it as the last child of a <div> with the ID "container".*/
+
+const paragraph = document.createElement("p");
+paragraph.innerText= "learning how to create elements and append.";
+container.append(paragraph);
+
+/*Select the second <li> element in a list and remove it from the DOM.*/
+const list = document.querySelector("li").nextElementSibling;
+list.remove();
+
+/*Select a <div> with the ID "main". Log all its direct child elements to the console, 
+ and then remove the first child from the list of children. */
+const children = document.querySelector("#main").children;
+children[0].remove();
+for (let child of children) {
+  console.log(child);
 }
